83d35c9d0d59fde5414a763a59c43ac22d63a961
tarun.amasa@duke.edu <tarun.amasa@duke.edu>
2022-04-26 00:24:09 -0400

    added flip port

--- server.js
+++ server.js
@@ -11,6 +11,36 @@ const server = app.listen(port, () => {
 });
 
 console.log(coinFlip())
+// app.use(function(req,res) {
+//     res.status(404).send('404 NOT FOUND')
+// })
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
+
+app.get('/', (req,res) => {
+    res.send('welcome to the api!')
+})
+
+app.get('/app/flip', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.json({flip: coinFlip()})
+    
+        
+    });
+
+
+
+
 app.use(function(req,res) {
     res.status(404).send('404 NOT FOUND')
 })
